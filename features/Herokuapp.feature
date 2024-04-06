1e3e68645b99ac8d0d811c706d7715a53d09c0ca
MarcusPindardBaden <pindardbadenmarcus@gmail.com>
2024-04-06 15:43:16 +0100

    First commit in challenge

--- features/Herokuapp.feature
+++ features/Herokuapp.feature
@@ -6,3 +6,17 @@ Feature: The Internet Guinea Pig Website
     When I click on AB test
     Then I should see the AB test page
 
+
+    Scenario Outline: As a user, I can view a specific element in the challengeDom table
+
+    Given I am on the heroku app homepage
+    When I click on challenge Dom
+    Then I should see the challenge Dom page
+    And I can verify that I see "<value>" in the correct "<row>" and "<column>"
+    Examples:
+    | value | row | column |
+    | Apeirian7 | 7 | Ipsum | 
+    | Consequuntur4 | 4 | Amet |
+    | Definiebas1 | 1 | Sit |
+
+
--- features/step-definitions/steps.js
+++ features/step-definitions/steps.js
@@ -12,16 +12,27 @@ const pages = {
     abTest: ABTestPage
 }
 
-Given("I am on the heroku app homepage", async () => {
-    browser.url("https://the-internet.herokuapp.com/")
-});
+    Given("I am on the heroku app homepage", async () => {
+        browser.url("https://the-internet.herokuapp.com/")
+    });
 
-When("I click on AB test", async () => {
-        await pages.home.abLink.click();
-});
+    When("I click on AB test", async () => {
+            await pages.home.abLink.click();
+    });
 
-Then("I should see the AB test page", async () => {
-    await expect(pages.abTest.content).toBeExisting();
-    await expect(pages.abTest.content).toHaveTextContaining('A/B Test Control');
-});
+    Then("I should see the AB test page", async () => {
+        await expect(pages.abTest.content).toBeExisting();
+        await expect(pages.abTest.content).toHaveTextContaining('A/B Test Control');
+    });
+
+
+
+    When("I click on challenge Dom", async () => {
+    });
+
+    Then("I should see the challenge Dom page", async () => {
+    });
+
+    Then("I can verify that I see {string} in the correct {string} and {string}", async (string, string2, string3) => {
+        });
 
