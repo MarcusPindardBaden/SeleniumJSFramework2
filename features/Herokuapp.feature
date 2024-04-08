05a33c94e748b66d7e79c85b0b09e536fb555826
MarcusPindardBaden <pindardbadenmarcus@gmail.com>
2024-04-08 13:31:27 +0100

    completed test scenario for challenge Dom

--- features/Herokuapp.feature
+++ features/Herokuapp.feature
@@ -6,17 +6,18 @@ Feature: The Internet Guinea Pig Website
     When I click on AB test
     Then I should see the AB test page
 
-
+@current
     Scenario Outline: As a user, I can view a specific element in the challengeDom table
 
     Given I am on the heroku app homepage
     When I click on challenge Dom
     Then I should see the challenge Dom page
-    And I can verify that I see "<value>" in the correct "<row>" and "<column>"
+    And I can verify that I see the value "<value>" in the correct row "<row>" 
+    And I can verify that I see the value "<value>" in the correct column "<column>"
     Examples:
     | value | row | column |
-    | Apeirian7 | 7 | Ipsum | 
-    | Consequuntur4 | 4 | Amet |
-    | Definiebas1 | 1 | Sit |
+    | Apeirian7 | 8 | Ipsum | 
+    | Consequuntur4 | 5 | Amet |
+    | Definiebas1 | 2 | Sit |
 
 
--- features/step-definitions/steps.js
+++ features/step-definitions/steps.js
@@ -1,15 +1,15 @@
 const { Given, When, Then } = require('@wdio/cucumber-framework');
 const { expect, $ } = require('@wdio/globals')
 const LoginPage = require('../pageobjects/login.page');
-const SecurePage = require('../pageobjects/secure.page');
 const HomePage = require('../pageobjects/home.page');
 const ABTestPage = require('../pageobjects/abtest.page');
-const abtestPage = require('../pageobjects/abtest.page');
+const ChallengeDomPage = require('../pageobjects/challengedom.page');
 
 const pages = {
     login: LoginPage,
     home: HomePage,
-    abTest: ABTestPage
+    abTest: ABTestPage,
+    challengeDom: ChallengeDomPage
 }
 
     Given("I am on the heroku app homepage", async () => {
@@ -28,11 +28,21 @@ const pages = {
 
 
     When("I click on challenge Dom", async () => {
+        (await pages.home.challengeDomLink).click();
     });
 
     Then("I should see the challenge Dom page", async () => {
+        await expect(pages.challengeDom.content).toBeExisting();
+        await expect(pages.challengeDom.content).toHaveTextContaining('Challenging DOM');
     });
 
-    Then("I can verify that I see {string} in the correct {string} and {string}", async (string, string2, string3) => {
-        });
+    Then("I can verify that I see the value {string} in the correct row {string}", async (string, string2) => {
+            let tableRow = await pages.challengeDom.findTableRowOfElement(string);
+            await expect(tableRow).toEqual(string2);
+    });
+
+    Then("I can verify that I see the value {string} in the correct column {string}", async (string, string2) => {
+        let ColumnHeader = await pages.challengeDom.findTableColumnHeaderOfElement(string);
+        await expect(ColumnHeader).toEqual(string2);
+});
 
